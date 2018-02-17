69f709b51db1109db27fdfcc0496f81fffce53e3
Charles Nicholson <cnich43@gmail.com>
2018-02-17 15:00:15 -0500

    Changed build.bat to provide SDL2 to those who wouldn't have it configured.

--- build.bat
+++ build.bat
@@ -4,7 +4,11 @@ set CommonCompilerFlags=-MTd -nologo -fp:fast -Gm- -GR- -EHa- -Od -Oi -WX -W4 -w
 set CommonLinkerFlags= -incremental:no -opt:ref /SUBSYSTEM:CONSOLE SDL2main.lib SDL2.lib
 
 IF NOT EXIST ..\..\build mkdir ..\..\build
+xcopy /y .\dependencies\SDL2.dll ..\..\build
+xcopy /y .\dependencies\SDL2.lib ..\..\build
+xcopy /y .\dependencies\SDL2main.lib ..\..\build
 pushd ..\..\build
+
 
 cl %CommonCompilerFlags% ..\code\ch8.c ..\code\cpu.c -Fmch8.map /link %CommonLinkerFlags%
 popd
